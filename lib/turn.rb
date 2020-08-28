9a5ec532d7dc81abb3029200cdb277a8e3ef87ed
Mimi C <mina123fire@yahoo.com>
2020-08-28 23:34:22 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board,index)
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def input_to_index(input)
+  input.to_i-1
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  turn = gets.strip
+end
+
+def position_taken?(board,index)
+  if board[index]==" "||board[index]==""||board[index]==nil
+    return FALSE
+  elsif board[index]=="X"||board[index]=="O"
+    return true
+  end
+end
+
+def move(board, index, player)
+  board[index] = " #{player} "
+end
